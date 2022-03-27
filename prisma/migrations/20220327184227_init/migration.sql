1264d1c04a734f313ee0a71eca462ab8bb9b0d7b
ayeolakenny <ayeolakenny@gmail.com>
2022-03-27 19:46:35 +0100

    init

--- prisma/migrations/20220327184227_init/migration.sql
+++ prisma/migrations/20220327184227_init/migration.sql
@@ -56,6 +56,35 @@ CREATE TABLE "Transaction" (
     CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "gameId" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "defaultCurrrency" "CURRENCY" NOT NULL DEFAULT E'USD',
+    "availability" INTEGER NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" SERIAL NOT NULL,
+    "subject" TEXT NOT NULL,
+    "expire" TIMESTAMP(3) NOT NULL,
+    "validity" BOOLEAN NOT NULL,
+    "mobileNumber" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
@@ -68,8 +97,14 @@ CREATE UNIQUE INDEX "User_mobileNumber_key" ON "User"("mobileNumber");
 -- CreateIndex
 CREATE UNIQUE INDEX "Wallet_userId_key" ON "Wallet"("userId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Game_gameId_key" ON "Game"("gameId");
+
 -- AddForeignKey
 ALTER TABLE "Wallet" ADD CONSTRAINT "Wallet_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
