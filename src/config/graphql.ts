a8aefcebe54e5097fd3132752eb708e61f00529f
ayeolakenny <ayeolakenny@gmail.com>
2022-05-02 11:26:42 +0100

    cors on rest fixes

--- src/config/graphql.ts
+++ src/config/graphql.ts
@@ -17,15 +17,16 @@ export class GqlConfigService implements GqlOptionsFactory {
       driver: ApolloDriver,
       autoSchemaFile: true,
       cors: {
-        origin: [
-          'https://betascratch.herokuapp.com',
-          'http://localhost:3000',
-          'http://127.0.0.1:3000',
-          'https://beta-admin.vercel.app',
-          'https://betascratch.com',
-          'https://www.betascratch.com',
-        ],
+        // origin: [
+        //   'https://betascratch.herokuapp.com',
+        //   'http://localhost:3000',
+        //   'http://127.0.0.1:3000',
+        //   'https://beta-admin.vercel.app',
+        //   'https://betascratch.com',
+        //   'https://www.betascratch.com',
+        // ],
         credentials: true,
+        origin: true,
       },
       context: ({ req, res }: Context) => ({ req, res }),
 
--- src/main.ts
+++ src/main.ts
@@ -14,46 +14,49 @@ async function bootstrap() {
 
   app.set('trust proxy', 1);
 
-    const origin = [
-      'https://betascratch.herokuapp.com',
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      'https://beta-admin.vercel.app',
-      'https://betascratch.com',
-      'https://www.betascratch.com',
-    ]
+  // const origin = [
+  //   'https://betascratch.herokuapp.com',
+  //   'http://localhost:3000',
+  //   'http://127.0.0.1:3000',
+  //   'https://beta-admin.vercel.app',
+  //   'https://betascratch.com',
+  //   'https://www.betascratch.com',
+  // ];
 
+  // app.enableCors({
+  //   allowedHeaders: [
+  //     'Origin',
+  //     'X-Requested-With',
+  //     'Content-Type',
+  //     'Accept',
+  //     'X-Access-Token',
+  //     'Authorization',
+  //     'Access-Control-Allow-Origin',
+  //     'Access-Control-Allow-Headers',
+  //   ],
+  //   credentials: true,
+  //   methods: ['GET', 'HEAD', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  //   origin,
+  //   preflightContinue: true,
+  // });
 
-  app.enableCors({
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'X-Access-Token',
-      'Authorization',
-      'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-    ],
-    credentials: true,
-    methods: ['GET', 'HEAD', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    origin,
-    preflightContinue: true,
-  })
-
-  app.use((req: Request, res: Response, next: NextFunction)=> {
-    if(origin.includes(req.headers.origin)){
-      res.header('Access-Control-Allow-Origin', req.headers.origin)
-    }
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD,PATCH');
-    res.setHeader('Access-Control-Allow-Headers',
-          'Origin, X-Requested-With,Content-Type,Accept,X-Access-Token,Authorization,Access-Control-Allow-Origin,Access-Control-Allow-Headers'
-    )
-    if(req.method === 'OPTIONS'){
-      return res.status(200).end()
-    }
-    return next()
-  })
+  // app.use((req: Request, res: Response, next: NextFunction) => {
+  //   if (origin.includes(req.headers.origin)) {
+  //     res.header('Access-Control-Allow-Origin', req.headers.origin);
+  //   }
+  //   res.header(
+  //     'Access-Control-Allow-Methods',
+  //     'GET,PUT,POST,DELETE,OPTIONS,HEAD,PATCH',
+  //   );
+  //   res.setHeader(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin, X-Requested-With,Content-Type,Accept,X-Access-Token,Authorization,Access-Control-Allow-Origin,Access-Control-Allow-Headers',
+  //   );
+  //   if (req.method === 'OPTIONS') {
+  //     return res.status(200).end();
+  //   }
+  //   return next();
+  // });
 
   // app.enableCors({
   //   origin: [
@@ -71,6 +74,12 @@ async function bootstrap() {
   //   credentials: true,
   //   // preflightContinue: true,
   // });
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
