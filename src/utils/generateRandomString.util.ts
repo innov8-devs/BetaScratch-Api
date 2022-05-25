3cae15723c75c99389bf469c4e6ddc7a83868fe2
ayeolakenny <ayeolakenny@gmail.com>
2022-05-25 08:49:36 +0100

    changed reference to nanoid

--- src/utils/generateRandomString.util.ts
+++ src/utils/generateRandomString.util.ts
@@ -1,12 +1,7 @@
+import { nanoid } from 'nanoid';
+
 export const generateRandomNumbers = () => {
   return Math.floor(1000 + Math.random() * 9000);
 };
 
-import { v4 } from 'uuid';
-
-export const generateRandomString = (): string => {
-  let hexString = v4();
-  hexString = hexString.replace(/-/g, '');
-  let base64String = Buffer.from(hexString, 'hex').toString('base64');
-  return base64String;
-};
+export const generateRandomString = (): string => nanoid(8);
