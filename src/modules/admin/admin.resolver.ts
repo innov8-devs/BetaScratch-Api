203241c678813f13c68f95ed4c4c8f26d6af00ad
ayeolakenny <ayeolakenny@gmail.com>
2022-07-28 14:51:11 +0100

    bank stuff

--- src/modules/admin/admin.resolver.ts
+++ src/modules/admin/admin.resolver.ts
@@ -370,7 +370,7 @@ export class AdminResolver {
   }
 
   @Auth([ROLE.ADMIN])
-  @Query(() => Purchase)
+  @Query(() => [Purchase])
   async fetchBankTransferPurchase(
     @Args('input') input: PaginationInput,
     @CurrentUser() user: User,
